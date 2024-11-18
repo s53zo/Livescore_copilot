0520caad0fa65a96fe15ff19157d07d070946313
s53zo <44831766+s53zo@users.noreply.github.com>
2024-11-18 23:07:16 +0100

    Update score_reporter.py

--- score_reporter.py
+++ score_reporter.py
@@ -340,8 +340,7 @@ class ScoreReporter:
                         query += f" AND {field} = ?"
                         params.append(filter_value)
     
-                query += " ORDER BY score DESC LIMIT 50"
-                #query += " ORDER BY score"
+                query += " ORDER BY score"
 
                 
                 # Log the query for debugging
