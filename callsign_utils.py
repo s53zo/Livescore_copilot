692664ef19b3515d6e09346afcae0a97db6ea370
s53zo <44831766+s53zo@users.noreply.github.com>
2024-12-03 08:32:38 +0100

    Update callsign_utils.py

--- callsign_utils.py
+++ callsign_utils.py
@@ -4,50 +4,45 @@ class CallsignLookup:
         self._cache: Dict[str, dict] = {}
         
     def get_callsign_info(self, callsign: str) -> Optional[dict]:
-        """Get callsign info, using the main DXCC prefix from CTY.plist"""
+        """Get callsign info, always using the Prefix field from CTY.plist entry"""
         if callsign in self._cache:
             return self._cache[callsign]
         
         base_call = callsign.split('/')[0]
+        info = None
         
         # First check for exact callsign match
         if base_call in self.cty_list:
-            info = self.cty_list[base_call].copy()
-            result = self._create_result_dict(info)
+            info = self.cty_list[base_call]
+        else:
+            # Then check for prefix match
+            for i in range(len(base_call), 0, -1):
+                prefix = base_call[:i]
+                if prefix in self.cty_list:
+                    info = self.cty_list[prefix]
+                    break
+        
+        if info:
+            result = {
+                "prefix": info.get("Prefix", ""),  # Use the Prefix field from CTY.plist
+                "country": info.get("Country", ""),
+                "continent": info.get("Continent", ""),
+                "adif": info.get("ADIF", 0),
+                "cq_zone": info.get("CQZone", 0),
+                "itu_zone": info.get("ITUZone", 0),
+                "latitude": info.get("Latitude", 0.0),
+                "longitude": info.get("Longitude", 0.0)
+            }
             self._cache[callsign] = result
             return result
-            
-        # Then check for prefix match
-        for i in range(len(base_call), 0, -1):
-            prefix = base_call[:i]
-            if prefix in self.cty_list:
-                info = self.cty_list[prefix].copy()
-                result = self._create_result_dict(info)
-                self._cache[callsign] = result
-                return result
         
         self._cache[callsign] = None
         return None
 
-    def _create_result_dict(self, info: dict) -> dict:
-        """Create result dictionary using the main DXCC prefix from CTY.plist"""
-        return {
-            "prefix": info.get("Prefix", ""),  # This will be the main DXCC prefix
-            "country": info.get("Country", ""),
-            "continent": info.get("Continent", ""),
-            "adif": info.get("ADIF", 0),
-            "cq_zone": info.get("CQZone", 0),
-            "itu_zone": info.get("ITUZone", 0),
-            "latitude": info.get("Latitude", 0.0),
-            "longitude": info.get("Longitude", 0.0)
-        }
-
     def clear_cache(self) -> None:
-        """Clear the lookup cache"""
         self._cache.clear()
 
     def _load_plist(self, plist_path: str) -> dict:
-        """Load and parse the CTY.plist file"""
         try:
             with open(plist_path, 'rb') as file:
                 return plistlib.load(file)
